89985b635469c49263e905a829983de957d964b1
wannabewize <wannabewize@gmail.com>
2018-06-05 01:53:46 +0900

    소켓 서버

--- Networking/Socket/server.js
+++ Networking/Socket/server.js
@@ -4,53 +4,31 @@ var clientList = [];
 
 var server = net.createServer( socket => {
    // Connection Event
-   console.log('Remote Address : ', socket.remoteAddress);
+   console.log('Client Connected from : ', socket.remoteAddress);
+
+   sendColor();
+
+   function sendColor() {
+      let random = Math.random() * 10;
+
+      setTimeout( () => {
+         let r = Math.floor(Math.random() * 255);
+         let g = Math.floor(Math.random() * 255);
+         let b = Math.floor(Math.random() * 255);
    
-   socket.write('Welcome to TCP Chat Service, ' + nickname + '\n');
-   socket.write('대화명 변경 : \\rename [NAME]\n');
-   socket.write('채팅방 종료 : \\exit\n');   
+         let color = { red: r, green: g, blue : b};
+         let data = JSON.stringify(color);
+         socket.write(data);
 
-   // 임시 닉네임 생성
-
-   var nickname = 'Guest' + Math.floor(Math.random()*100);
-   clientList.push(socket);
+         sendColor();
+      }, random * 1000);
+   }
 
    socket.on('data', (data) => {
-         console.log('socket is nil?', !socket)
-      var message = data.toString('UTF-8');
-
-      if ( message.trim() == '\\exit' ) {
-         socket.end();
-         return;
-      }
-      else if ( message.indexOf('\\rename') != -1 ) {
-         var newNickName = message.split(' ')[1];
-         
-         message = nickname + ' change nickname ' + newNickName;
-         console.log(message);
-                  
-         nickname = newNickName;
-         return;         
-      }
-      else {
-         console.log(nickname + ' write ' + message);   
-         message = nickname + ' : ' + message;
-      }
-                  
-      // 채팅 메세지를 모든 클라이언트 소켓에 전달
-      for(var client of clientList) {
-         // 글 작성자에게는 보내지 않는다.
-         if ( socket != client )      
-         client.write(message);
-      };
    });
 
    socket.on('end', () => {
-      // 소켓 목록에서 삭제
-      var index = clientList.indexOf(socket);
-      clientList.splice(index, 1);
-      
-      console.log(nickname + "님이 채팅방을 떠났습니다.");
+      console.log('Connection end')
    });
 });
 
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # iOS_Useful_Libraries
 iOS 기반 유용한 라이브러리 예제
+
+
+## Networking
+
+네트워크 관련 라이브러리
+
+### Socket
+
+TCP 소켓을 이용해서 임의의 시간 간격으로 변경되는 값이 전달된다. 실시간으로 전달되는 값
+
+{
+   "red": 200,
+   "green": 128,
+   "blue", 184
+}
