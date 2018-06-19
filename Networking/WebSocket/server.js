8e4096ca6dd541276f717ce5d72847a50ed97447
wannabewize <wannabewize@gmail.com>
2018-06-20 06:27:53 +0900

    웹 소켓 예제

--- Networking/WebSocket/server.js
+++ Networking/WebSocket/server.js
@@ -1,3 +1,13 @@
+const express = require('express');
+const app = express();
+app.listen(3000, () => {
+    console.log('server is running @3000');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
 const WebSocket = require('ws');
 
 const ws = new WebSocket.Server({port: 8080});
